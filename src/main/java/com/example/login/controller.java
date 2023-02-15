466e984f6862fd7c71f8afd2c7f8dcf000fca216
Dilka Sithari <sitharidilka@gmail.com>
2023-02-15 14:32:49 +0530

    authontication and authorization added

--- src/main/java/com/example/login/controller.java
+++ src/main/java/com/example/login/controller.java
@@ -8,6 +8,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class controller {
     @GetMapping("/")
     public String home(){
-        return "This is home page";
+        return "<h1>This is home page</h1>";
+    }
+
+    @GetMapping("/user")
+    public String user(){
+        return "<h1>This is User page</h1>";
+    }
+
+    @GetMapping("/admin")
+    public String admin(){
+        return "<h1>This is Admin page</h1>";
     }
 }
